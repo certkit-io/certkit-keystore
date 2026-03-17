745488fb5f7d4adf8ad6b5477125cdbb3dd2bf8f
Eric Brandes <eric.brandes@gmail.com>
2026-03-17 11:50:24 -0500

    Another place where we need the pending CSR status

--- cmd/certkit-keystore/run.go
+++ cmd/certkit-keystore/run.go
@@ -138,6 +138,9 @@ func processPollResponse(resp *api.PollResponse) {
 				} else {
 					log.Printf("Failed to match CSR key for %s: %v", cert.CustomCertId, cert.LatestIssuedCert)
 				}
+			} else if cert.LatestIssuedCert.Key == "" && keyOnDisk && storage.HasPendingCSR(cert.CustomCertId) {
+				statuses = append(statuses, api.UpdateStatusItem{CustomCertId: cert.CustomCertId, Status: api.CertStatuses.PendingCSR})
+				continue
 			}
 
 			wrote, err := storage.EnsureCertOnDisk(cert.CustomCertId, cert.LatestIssuedCert)
